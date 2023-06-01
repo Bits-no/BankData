70ab2ebae8d3d17b8af7d4193f59c163b2ca5284
Christian I. Nilsson <nikize@gmail.com>
2023-06-01 17:22:37 +0200

    .NET Tests some Validation of data

---
+++ NET/BitsNo.Tests/Helpers.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework.Constraints;
+using System.Text;
+
+namespace BitsNo.Tests;
+
+internal static class Helpers
+{
+    public static DirectoryInfo GetBaseDir()
+    {
+        var ncruncProj = Environment.GetEnvironmentVariable("NCrunch.OriginalProjectPath");
+        var ncruncProjDi = ncruncProj == null ? null : new FileInfo(ncruncProj).Directory;
+        var di = ncruncProjDi ?? new DirectoryInfo(Directory.GetCurrentDirectory());
+        while (true)
+        {
+            Console.WriteLine($" {nameof(GetBaseDir)} Looking for Data dir .. {di}");
+            var diData = di.EnumerateDirectories("Data").FirstOrDefault();
+            if (diData != null)
+                return diData;
+            di = di.Parent;
+            if (di == null)
+                throw new Exception("Data spath not found");
+        }
+    }
+
+    public static Constraint ContainsAnyUmlaut()
+    {
+        Constraint x = Does.Contain("Å");
+        foreach (var c in "ÄØåäø")
+            x = x.Or.Contain(c);
+        return x;
+    }
+
+    public static IEnumerable<string> GetLines(string data) =>
+        data.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None)
+        .Select(l => l.Trim())
+        .Where(l => l.Length != 0 && l[0] != '#');
+
+    public static string FindAndReadTextFile(string filename)
+    {
+        var fi = GetBaseDir().EnumerateFiles(filename).First();
+        return File.ReadAllText(fi.FullName, Encoding.UTF8);
+    }
+}
---
+++ NET/BitsNo.Tests/SimpleValidationsTests.cs
@@ -0,0 +1,21 @@
+namespace BitsNo.Tests;
+
+public class SimpleValidationsTests
+{
+    [Test]
+    public void SourcePsvHasDataTest() =>
+        Assert.That(Data.Banks.SourcePsv, Is.Not.Null.Or.Empty,
+            Data.Banks.SourcePsv);
+
+    [TestCase("source.psv")]
+    public void SourceFileHasCorrectEncoding(string fileName)
+    {
+        var fileData = Helpers.FindAndReadTextFile(fileName);
+        Assert.That(fileData, Helpers.ContainsAnyUmlaut(),
+            fileData);
+    }
+
+    [Test]
+    public void SourcePsvHasCorrectEncoding() =>
+        Assert.That(Data.Banks.SourcePsv, Helpers.ContainsAnyUmlaut());
+}
